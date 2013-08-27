3e97f4266452aa2d53aa231570d065cc53753180
sandbox-admin <sandbox-admin@sandbox-2.net>
2013-08-27 22:25:10 +0200

    Ajout du réseau par défaut

--- tisseo-s/src/main/scala/net/rsemlal/tisseos/data/objects/Network.scala
+++ tisseo-s/src/main/scala/net/rsemlal/tisseos/data/objects/Network.scala
@@ -1,5 +1,17 @@
 package net.rsemlal.tisseos.data.objects
 
+object Network {
+  /**
+   * Réseau Tisseo.
+   */
+  object Tisseo extends Network("Tisséo")
+
+  /**
+   * Réseau par défaut.
+   */
+  val Default = Tisseo
+}
+
 /**
  * Réseau.
  * @param name Nom du réseau de transport.
